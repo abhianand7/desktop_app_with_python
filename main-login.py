7ac5e3c2784cdf149d1ed48e1dbfdbaf81314b50
Abhinav Anand <erabhinav@outlook.com>
2016-07-22 16:17:47 +0530

    edited

--- main-login.py
+++ main-login.py
@@ -19,7 +19,6 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.button import Button
-# from kivy.uix.popup import Popup
 import popup
 
 # custom imports
